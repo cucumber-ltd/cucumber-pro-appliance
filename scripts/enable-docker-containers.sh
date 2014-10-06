7dc2a2689e8b85dcd91f532c8601ff1b170bf0df
Julien Biezemans <jb@jbpros.com>
2014-10-06 13:52:15 +0200

    Enable docker containers

--- scripts/enable-docker-containers.sh
+++ scripts/enable-docker-containers.sh
@@ -6,10 +6,19 @@
 #
 set -e pipefail
 
-gunzip -c images/mongo.tar.gz | docker load
-gunzip -c images/postgres.tar.gz | docker load
-gunzip -c images/redis.tar.gz | docker load
+# remove all images to avoid weird btrfs-related bug:
+docker images -a -q | xargs docker rmi
+echo "Loading mongo"
+gunzip -c images/mongo.tar.gz > images/mongo.tar
+docker load -i images/mongo.tar
+echo "Loading postgres"
+gunzip -c images/postgres.tar.gz > images/postgres.tar
+docker load -i images/postgres.tar
+echo "Loading redis"
+gunzip -c images/redis.tar.gz > images/redis.tar
+docker load -i images/redis.tar
 docker images
 sudo systemctl enable /etc/systemd/system/docker-mongo.service
 sudo systemctl enable /etc/systemd/system/docker-postgres.service
 sudo systemctl enable /etc/systemd/system/docker-redis.service
+rm images/*.tar
