86a9d49ef16d785159a93b46ccfc3d2652a2e486
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2014-10-28 14:37:26 +0000

    Fix bug in postgres data path

--- images/start-postgres.sh
+++ images/start-postgres.sh
@@ -3,5 +3,5 @@ DOCKER_TAG=$1
 /usr/bin/docker run --rm \
   --name postgres \
   -p 5432:5432 \
-  -v /core/core/volumes/postgres/data:/var/lib/postgresql/data \
+  -v /core/volumes/postgres/data:/var/lib/postgresql/data \
   postgres:$DOCKER_TAG
